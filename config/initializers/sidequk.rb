ea139002a8ffd8af4b0af0b9ef92179049c8f688
Valerii Kuzin <kuzin.valeriy@gmail.com>
2020-08-26 10:14:43 +0300

    #000 Fixed redis index error

--- config/initializers/sidequk.rb
+++ config/initializers/sidequk.rb
@@ -1,7 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch("REDISCLOUD_URL") { "redis://localhost:6379" + '/0' } }
+  config.redis = { url: ENV.fetch("REDISCLOUD_URL") { "redis://localhost:6379" } }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch("REDISCLOUD_URL") { "redis://localhost:6379" + '/0'} }
+  config.redis = { url: ENV.fetch("REDISCLOUD_URL") { "redis://localhost:6379"} }
 end
