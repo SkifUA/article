4b3027dc9138ed4e55cde38553cd0013db45cfae
Valerii Kuzin <kuzin.valeriy@gmail.com>
2020-08-31 07:46:53 +0300

    000 Added GET default route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   mount ActionCable.server => '/cable'
+  root to: 'application#index'
   resources :stories
   resources :articles
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
